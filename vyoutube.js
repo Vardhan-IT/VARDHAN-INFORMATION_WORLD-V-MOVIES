a09175d645d417e51e9c9958e5bd400836761385
Vardhan-IT <162978108+Vardhan-IT@users.noreply.github.com>
2024-03-12 15:50:01 +0530

    Update vyoutube.js

--- vyoutube.js
+++ vyoutube.js
@@ -2,31 +2,31 @@ let videos=[
     {
         name:"Avatar-Telugu-2009(mp4)",
         thumbnail:"Avatar-Telugu-2009(mp4).jpg",
-        video:"videos/Avatar-Telugu-2009.mp4"
+        video:"Avatar-Telugu-2009.mp4"
     },
     {
         name:"777-Charlie-Telugu-h1tfe-2022(mp4)",
         thumbnail:"777-Charlie-Telugu-h1tfe-2022(mp4).jpg",
-        video:"videos/777-Charlie-Telugu-h1tfe-2022.mp4"
+        video:"777-Charlie-Telugu-h1tfe-2022.mp4"
     },
     {
         name:"Brahmastra-Part-One-Shiva-Telugu-n3sdf-2022(mp4)",
         thumbnail:"Brahmastra-Part-One-Shiva-Telugu-n3sdf-2022(mp4).webp",
-        video:"videos/Brahmastra-Part-One-Shiva-Telugu-n3sdf-2022.mp4"
+        video:"Brahmastra-Part-One-Shiva-Telugu-n3sdf-2022.mp4"
     },
     {
         name:"Captain-Telugu-n2dsf-2022(mp4)",
         thumbnail:"Captain-Telugu-n2dsf-2022(mp4).jpg",
-        video:"videos/Captain-Telugu-n2dsf-2022.mp4"
+        video:"Captain-Telugu-n2dsf-2022.mp4"
     },
     {
         name:"Crazy-Fellow-Telugu-v2zsf-2022(mp4)",
         thumbnail:"Crazy-Fellow-Telugu-v2zsf-2022(mp4).jpg",
-        video:"videos/Crazy-Fellow-Telugu-v2zsf-2022.mp4"
+        video:"Crazy-Fellow-Telugu-v2zsf-2022.mp4"
     },
     {
         name:"Gandharwa-Telugu-shb6c-2022(mp4)",
         thumbnail:"Gandharwa-Telugu-shb6c-2022(mp4).jpg",
-        video:"videos/Gandharwa-Telugu-shb6c-2022.mp4"
+        video:"Gandharwa-Telugu-shb6c-2022.mp4"
     }
 ];
